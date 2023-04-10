4a392a177d60ea81e3338a32f3e446c2bcf48266
Andrey_Bryzgalov <bav03630363@gmail.com>
2023-04-10 19:23:32 +0300

    Save 19:23 10.04.23

--- Lesson_1/Program.cs
+++ Lesson_1/Program.cs
@@ -17,23 +17,35 @@
 
 // Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-Console.Write("Введите первое число: ");
+// Console.Write("Введите первое число: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите третье число: ");
+// int number3 = Convert.ToInt32(Console.ReadLine());
+
+// int max_number = number1;
+
+// if(number2 > max_number)
+// {
+//     max_number = number2;
+// }
+
+// if(number3 > max_number)
+// {
+//     max_number = number3;
+// }
+// Console.WriteLine("Максимальное значение из введенных чисел: " + max_number);
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.Write("Введите число: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе число: ");
-int number2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите третье число: ");
-int number3 = Convert.ToInt32(Console.ReadLine());
-
-int max_number = number1;
-
-if(number2 > max_number)
+if(number1 % 2 == 0)
 {
-    max_number = number2;
+    Console.WriteLine("Введенное число является четным");
 }
-
-if(number3 > max_number)
+else
 {
-    max_number = number3;
+    Console.WriteLine("Введенное число является нечетным");
 }
-Console.WriteLine("Максимальное значение из введенных чисел: " + max_number);
-
