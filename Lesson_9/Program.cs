5cbba6249fde53278a880abbf0e07d8771e25922
Andrey_Bryzgalov <bav03630363@gmail.com>
2023-05-29 21:49:42 +0300

    save 29.05.2023 21:49

--- Lesson_9/Program.cs
+++ Lesson_9/Program.cs
@@ -5,17 +5,10 @@
     return result;
 }
 
-int[] FromNto1(int n)
+void FromNto1(int n) // метод выводит значения от n до 1
 {
-    int[] array = new int[n];
-
-    for (int index = 0; index < array.Length; index++)
-    {
-        array[index] = n - index;
-        Console.Write(array[index] + " ");
-    }
-    Console.WriteLine();
-    return array;
+    Console.Write(n + " ");
+    if (n > 1) FromNto1(n - 1);
 }
 
 int[,] DeleteRowAndCol(int[,] array) // Метод находит наименьший элемент массива и удаляет из масива строку и столбец с данным элементом
@@ -49,9 +42,38 @@ int[,] DeleteRowAndCol(int[,] array) // Метод находит наимень
     return rezultArray;
 }
 
+int SumMtoN(int m, int n) // Метод находит сумму в промежутке заданных чисел
+{
+    if (m < n) return SumMtoN(m + 1, n) + m;
+    else if (m > n) return SumMtoN(m, n + 1) + n;
+    else return m;
+}
 
+int Ackerman(int n, int m) // Метод Аккермана
+{
+    if (n == 0) return m + 1;
+    if (n != 0 && m == 0) return Ackerman(n - 1, 1);
+    if (n > 0 && m > 0) return Ackerman(n - 1, Ackerman(n, m - 1));
+    return Ackerman(n, m);
+}
 
-int n = Input("Введите кол-во строк в массиве: ");
-int[] array = FromNto1(n);
+// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+//Выполнить с помощью рекурсии.
+
+// int n = Input("Введите число: ");
+// FromNto1(n);
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// int m = Input("Введите число m: ");
+// int n = Input("Введите число n: ");
+// Console.WriteLine($"Сумма элементов равна {SumMtoN(m, n)}");
+
+//Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+// int m = Input("Введите число m: ");
+// int n = Input("Введите число n: ");
+// Console.WriteLine($"Функция Аккермана равна {Ackerman(m, n)}");
+
 
 
